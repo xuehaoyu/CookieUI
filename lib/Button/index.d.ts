a363c716c607a2480c26e477f0e80f3d9b69dda9
xuehaoyu <1169732429@qq.com>
2021-10-25 15:02:40 +0800

    add lib

---
+++ lib/Button/index.d.ts
@@ -0,0 +1,31 @@
+import React from 'react';
+export interface IButtonProps {
+    onClick?: React.MouseEventHandler;
+    primary?: boolean;
+    secondary?: boolean;
+    outline?: boolean;
+    dashed?: boolean;
+    link?: boolean;
+    text?: boolean;
+    round?: boolean;
+    noRadius?: boolean;
+    fat?: boolean;
+    xLarge?: boolean;
+    large?: boolean;
+    small?: boolean;
+    xSmall?: boolean;
+    xxSmall?: boolean;
+    long?: boolean;
+    short?: boolean;
+    success?: boolean;
+    warn?: boolean;
+    danger?: boolean;
+    disabled?: boolean;
+    href?: string;
+    className?: string;
+    style?: React.CSSProperties;
+    children?: React.ReactNode;
+}
+/** 按钮 */
+declare const Button: React.FC;
+export default Button;
