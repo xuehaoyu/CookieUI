ad008a855903e66ba15d1101d76c263e82e10f55
xuehaoyu <1169732429@qq.com>
2021-10-25 14:33:48 +0800

    add index.js

---
+++ index.js
@@ -0,0 +1,5 @@
+if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
+  module.exports = require("./dist/cookie.js");
+} else {
+  module.exports = require("./dist/cookie.min.js");
+}
